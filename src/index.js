d7e433f96f82e439bb0aba29c7a918ea81dc5f66
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2021-08-11 22:36:35 +0300

    feat: implement min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+	if ((arguments.length == 0) || (array.length == 0)) {
+		return 0;
+	}
+	let minValue = array[0];
+	for (let i = 1; i < array.length; i++) {
+		if (array[i] < minValue) 
+			minValue = array[i];
+	}
+   return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+	if ((arguments.length == 0) || (array.length == 0)) {
+		return 0;
+	}
+	let maxValue = array[0];
+	for (let i = 1; i < array.length; i++) {
+		if (array[i] > maxValue) 
+			maxValue = array[i];
+	}
+   return maxValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if ((arguments.length == 0) || (array.length == 0)) {
+		return 0;
+	}
+  let avgValue = array.reduce((sum, current) => sum + current, 0) / array.length;
+  /*for (let i = 0; i < array.length; i++) {
+	  sum += array[i];
+  }
+  let avgValue = sum / array.length; */
+  return avgValue;
 }
